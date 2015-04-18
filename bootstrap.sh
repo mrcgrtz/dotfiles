d2c55d2489aff57c1c62762bb8f4c7d9ab988108
Marc Görtz <mail@marcgoertz.de>
2015-04-18 05:29:53 +0200

    Better exclusion.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,8 +5,8 @@ cd "$(dirname "${BASH_SOURCE}")";
 git pull origin master;
 
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
+	rsync --exclude ".git/" --exclude "init/" --exclude "bin/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+		--exclude "README.md" --exclude "MIT-LICENSE.txt" -avh --no-perms . ~;
 	source ~/.bash_profile;
 }
 
