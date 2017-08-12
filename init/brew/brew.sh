ec3dde0aa64fcdc07d6aad0db892861b561c81c4
Marc Görtz <mail@marcgoertz.de>
2017-08-12 13:47:09 +0200

    Use spaces instead of tabs in dotfiles.

--- init/brew/brew.sh
+++ init/brew/brew.sh
@@ -18,8 +18,8 @@ ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
 
 # Switch to using brew-installed bash as default shell.
 if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
-	echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
-	chsh -s /usr/local/bin/bash;
+  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
+  chsh -s /usr/local/bin/bash;
 fi;
 
 # Remove outdated versions from the cellar.
