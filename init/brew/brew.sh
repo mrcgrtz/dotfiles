1a0c9069ac7a06b2f40e58b1dc29e3a8370e8012
Marc Görtz <mail@marcgoertz.de>
2019-01-30 17:41:25 +0100

    Split Brewfile and ask for installing Casks.

--- init/brew/brew.sh
+++ init/brew/brew.sh
@@ -3,24 +3,31 @@
 # Install Homebrew (you need the Xcode CLI tools!)
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Make sure we’re using the latest Homebrew.
+# Make sure we are using the latest Homebrew.
 brew update
 
 # Upgrade any already-installed formulae.
 brew upgrade
 
-# Install formulae from Brewfile
-brew bundle
+# Install formulae from Brewfile.
+brew bundle --file=Brewfile
+
+# Maybe install formulae from Caskfile, too.
+read -p "Also install native apps using Homebrew Casks and the Mac App Store (y/N)?" docask
+case "$docask" in
+  y|yes ) brew bundle --file=Caskfile;;
+  * ) echo "Skipping Caskfile.";;
+esac
 
 # Use GNU tools like sha256sum.
-echo "Don’t forget to add $(brew --prefix coreutils)/libexec/gnubin to \$PATH."
+echo "Do not forget to add $(brew --prefix coreutils)/libexec/gnubin to \$PATH."
 ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
 
-# Switch to using brew-installed bash as default shell.
+# Switch to using Brew-installed Bash as default shell.
 if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
   echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
   chsh -s /usr/local/bin/bash;
 fi;
 
-# Remove outdated versions from the cellar.
+# Remove outdated versions from the Cellar.
 brew cleanup
