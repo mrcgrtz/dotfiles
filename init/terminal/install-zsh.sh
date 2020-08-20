b18ceb9ca7c92e3b429bfa541159d0fd4f853b40
Marc Görtz <mail@marcgoertz.de>
2020-08-20 13:51:43 +0200

    Update install script.

--- init/terminal/install-zsh.sh
+++ init/terminal/install-zsh.sh
@@ -4,9 +4,9 @@
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install plugins
-git clone git@github.com:zsh-users/zsh-autosuggestions.git "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
-git clone git@github.com:zsh-users/zsh-syntax-highlighting.git "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
 
 # Install the Pure prompt
 mkdir -p "$HOME/.zsh"
-git clone git@github.com:sindresorhus/pure.git "$HOME/.zsh/pure"
+git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
