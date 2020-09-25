b31b26852ac0935b651cc93e03080eadb8cf4a1a
Marc Görtz <mail@marcgoertz.de>
2020-09-25 14:25:20 +0200

    Update bootstrap script.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")";
+cd "$(dirname "${BASH_SOURCE}")" || exit;
 
 function doIt() {
   git pull origin main;
@@ -13,15 +13,32 @@ function doIt() {
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
   doIt;
 else
-  read -p "🍺  Install Homebrew and its formulae? (y/N) " dobrew
-  case "$dobrew" in
-    y|yes ) source init/brew/brew.sh;;
+  # Homebrew
+  read -r -p "🍺  Install Homebrew and its formulae? (y/N) " runswitch
+  case "$runswitch" in
+    y|yes ) source init/brew/install-brew-formulae.sh;;
     * ) echo "Skipping Homebrew.";;
   esac
-  read -p "🚨  Installing dotfiles. This will overwrite existing files in your home directory. Are you sure? (y/N) " dobrew
-  case "$dobrew" in
+
+  # dotfiles
+  read -r -p "🚨  Installing dotfiles. This will overwrite existing files in your home directory. Are you sure? (y/N) " runswitch
+  case "$runswitch" in
     y|yes ) doIt;;
     * ) echo "Skipping installation.";;
+  esac
+
+  # zsh
+  read -r -p "🙃  Install ohmyzsh and some plugins? (y/N) " runswitch
+  case "$runswitch" in
+    y|yes ) source init/terminal/install-zsh.sh;;
+    * ) echo "Skipping ohmyzsh.";;
+  esac
+
+  # Node modules
+  read -r -p "❇️  Install Node modules? (y/N) " runswitch
+  case "$runswitch" in
+    y|yes ) source init/npm/install-node-modules.sh;;
+    * ) echo "Skipping Node modules.";;
   esac
 fi;
 unset doIt;
