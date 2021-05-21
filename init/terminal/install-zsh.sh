078842889fe4787f7f712e02ed6fb1f420b7506d
Marc Görtz <mail@marcgoertz.de>
2021-05-21 17:26:07 +0200

    Add alias-tips plugin to zsh.

--- init/terminal/install-zsh.sh
+++ init/terminal/install-zsh.sh
@@ -4,8 +4,9 @@
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install plugins.
-git clone https://github.com/zsh-users/zsh-autosuggestions.git "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git "${ZSH_CUSTOM1:-$ZSH/custom}/plugins/zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM1:-$ZSH/custom}/plugins/zsh-syntax-highlighting"
+git clone https://github.com/djui/alias-tips.git "${ZSH_CUSTOM1:-$ZSH/custom}/plugins/alias-tips"
 
 # Install the Pure prompt.
 mkdir -p "$HOME/.zsh"
