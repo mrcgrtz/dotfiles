cdefe627b6796cf3b1a705693134e179b33813e6
Marc Görtz <mail@marcgoertz.de>
2021-12-28 14:00:09 +0100

    Install Pure via Homebrew.

--- init/terminal/install-zsh.sh
+++ init/terminal/install-zsh.sh
@@ -7,7 +7,3 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 git clone https://github.com/zsh-users/zsh-autosuggestions.git "${ZSH_CUSTOM1:-$ZSH/custom}/plugins/zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM1:-$ZSH/custom}/plugins/zsh-syntax-highlighting"
 git clone https://github.com/djui/alias-tips.git "${ZSH_CUSTOM1:-$ZSH/custom}/plugins/alias-tips"
-
-# Install the Pure prompt.
-mkdir -p "$HOME/.zsh"
-git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
