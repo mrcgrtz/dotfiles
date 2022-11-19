49d25395966bf4f494978c96753b83d8b7bec01f
Marc Görtz <mail@marcgoertz.de>
2022-11-19 01:13:53 +0100

    Update Node modules install script.

--- init/npm/install-node-modules.sh
+++ init/npm/install-node-modules.sh
@@ -3,34 +3,16 @@
 # Update npm itself.
 npm update -g npm
 
-# Install yarn (for some older projects).
-npm install -g yarn
+# Enable corepack (thus yarn).
+if ! command -v corepack > /dev/null 2>&1; then
+  npm install -g corepack
+fi;
+corepack enable
 
 # Install build tools.
 npm install -g gulp-cli
 npm install -g grunt-cli
 
-# Install bootstrappers.
-npm install -g create-react-app
-npm install -g @11ty/eleventy
-
-# Install linters.
-npm install -g eslint
-npm install -g stylelint
-npm install -g xo
-
-# Install analysis tools.
-npm install -g specificity-graph
+# Install performance analysis tools.
 npm install -g speed-test
-npm install -g stylestats
 npm install -g psi
-npm install -g tmi
-npm install -g hint
-
-# Install server development packages.
-npm install -g local-web-server
-npm install -g supervisor
-npm install -g easymock
-
-# Install funsies.
-npm install -g youtube-thumbnail
