e6d2b9c9094b517c0db9d0b65f7ec3387ad6e5e0
Marc Görtz <mail@marcgoertz.de>
2023-02-23 15:07:30 +0100

    Add links.

--- init/npm/install-node-modules.sh
+++ init/npm/install-node-modules.sh
@@ -10,12 +10,12 @@ fi;
 corepack enable
 
 # Install build tools.
-npm install -g gulp-cli
-npm install -g grunt-cli
+npm install -g gulp-cli     # https://www.npmjs.com/package/gulp-cli
+npm install -g grunt-cli    # https://www.npmjs.com/package/grunt-cli
 
 # Install performance analysis tools.
-npm install -g speed-test
-npm install -g psi
+npm install -g speed-test   # https://www.npmjs.com/package/speed-test
+npm install -g psi          # https://www.npmjs.com/package/psi
 
 # Install other useful utilities.
-npm install -g npm-check
+npm install -g npm-check    # https://www.npmjs.com/package/npm-check
