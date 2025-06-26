29cac27a53a204f03762000aaf660cb62453f109
Marc Görtz <mail@marcgoertz.de>
2025-06-26 23:59:22 +0200

    Add linters and formatters.

--- init/npm/install-node-modules.sh
+++ init/npm/install-node-modules.sh
@@ -16,5 +16,10 @@ npm install -g gulp-cli     # https://www.npmjs.com/package/gulp-cli
 npm install -g speed-test   # https://www.npmjs.com/package/speed-test
 npm install -g lighthouse   # https://www.npmjs.com/package/lighthouse
 
+# Install linters and formatters for Sublime Text.
+npm install -g eslint       # https://www.npmjs.com/package/eslint
+npm install -g stylelint    # https://www.npmjs.com/package/stylelint
+npm install -g prettier     # https://www.npmjs.com/package/prettier
+
 # Install other useful utilities.
 npm install -g npm-check    # https://www.npmjs.com/package/npm-check
